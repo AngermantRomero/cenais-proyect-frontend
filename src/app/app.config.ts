249ac12e8bd4ffff7028d5b749986410d03a1834
Wichy <wichy8780@gmail.com>
2025-05-24 15:52:48 -0400

    feat:inetrceptor

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,6 +1,7 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideAnimations } from '@angular/platform-browser/animations';
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { authInterceptor } from './core/interceptors/auth.interceptor'; 
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 
@@ -8,6 +9,7 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideAnimations(),
-    provideHttpClient(),    
+     provideHttpClient(withInterceptors([authInterceptor])),
+  
   ]
 };
